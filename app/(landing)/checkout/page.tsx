ca11e08142527b7a69ce00b7f0d30ae2343ad48a
putri_delta <putridelta0812@gmail.com>
2026-02-12 18:55:45 +0700

    [FIX] Integrasi checkout

--- app/(landing)/checkout/page.tsx
+++ app/(landing)/checkout/page.tsx
@@ -1,15 +1,38 @@
+"use client"
+import { useState } from "react";
 import CartItems from "../component/checkout/cart-items";
 import OrderInformation from "../component/checkout/order-information";
+import { CustomerInfo, useCartStore } from "@/app/hooks/use-cart-store";
+import { useRouter } from "next/navigation";
 
 
 const Checkout = () => {
+  const {push} = useRouter();
+  const {customerInfo, setCustomerInfo} = useCartStore();
+  const [formData, setFormData] = useState<CustomerInfo>({
+    customerName: "",
+    customerContact: null,
+    customerAddress: ""
+
+  });
+
+  const handlePayment = () => {
+    if (!formData.customerName || !formData.customerContact || !formData.customerAddress) {
+      alert("please fill in fields");
+      return;
+    }
+
+    setCustomerInfo(formData);
+    push("/payment");
+  };
+
   return (
-    <main className="bg-gray-100 min-h-[80vh]">
+    <main className="bg-gray-100 min-h-[80vh] pt-[50px]">
       <div className="max-w-5xl mx-auto py-15">
         <h1 className="text-5xl font-bold text-center mb-11">Checkout Now</h1>
         <div className="grid grid-cols-2 gap-14">
-          <OrderInformation/>
-          <CartItems/>
+          <OrderInformation formData={formData} setFormData={setFormData} />
+          <CartItems handlePayment={handlePayment}/>
         </div>
       </div>
     </main>
--- app/(landing)/component/checkout/cart-items.tsx
+++ app/(landing)/component/checkout/cart-items.tsx
@@ -1,31 +1,35 @@
 "use client";
 
 import Image from "next/image";
-import { cartList } from "../ui/cart-popup";
 import priceFormatter from "@/app/utils/price-formatter";
 import Button from "../ui/button";
 import { FiCreditCard, FiTrash2 } from "react-icons/fi";
 import CardWithHeader from "../ui/card-with-header";
 import { useRouter } from "next/navigation";
+import { useCartStore } from "@/app/hooks/use-cart-store";
+import { getImageUrl } from "@/app/lib/api";
 
-const CartItems = () => {
+type TcartItems ={
+  handlePayment: () => void;
+}
+const CartItems = ({handlePayment}: TcartItems) => {
+  const {items, removeItem} = useCartStore();
   const { push } = useRouter();
 
-  const totalPrice = cartList.reduce(
+  const totalPrice = items.reduce(
     (total, item) => total + item.price * item.qty,
     0
   );
 
-  const payment = () => {};
-
+  
   return (
     <CardWithHeader title="Cart Items">
       <div className="overflow-auto max-h-[300px]">
-        {cartList.map((item, index) => (
-          <div className="border-b border-gray-200 p-4 flex gap-3" key={index}>
+        {items.map((item, index) => (
+          <div className="border-b border-gray-200 p-4 flex gap-3" key={item._id}>
             <div className="bg-primary-light aspect-square w-16 flex justify-center items-center">
               <Image
-                src={`/images/products/${item.imgUrl}`}
+                src={getImageUrl(item.imageUrl)}
                 width={63}
                 height={63}
                 alt={item.name}
@@ -43,6 +47,7 @@ const CartItems = () => {
               size="small"
               variant="ghost"
               className="w-7 h-7 p-0! self-center ml-auto"
+              onClick={() => removeItem(item._id)}
             >
               <FiTrash2 />
             </Button>
@@ -60,7 +65,7 @@ const CartItems = () => {
         <Button
           variant="dark"
           className="w-full mt-4"
-          onClick={() => push("/payment")}
+          onClick={handlePayment}
         >
           <FiCreditCard />
           Proceed to Payment
